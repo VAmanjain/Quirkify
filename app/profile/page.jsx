6823221500573ca9fc39a0f60c1a4b4251edf08e
VAmanJain <vaman.jain17@gmail.com>
2024-03-09 14:35:28 +0530

    add secret

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
 
@@ -10,7 +10,7 @@ import Profile from "@components/Profile";
 const MyProfile = () => {
   const { data: session } = useSession();
   const [posts, setPosts] =useState([]);
-  const router = useRouter();
+  // const router = useRouter();
 
   useEffect(() => {
     const fetchPosts = async () => {
